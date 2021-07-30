fc7ba12d517c882a7c857a765be1075b9f4643ea
Adson Santos <adsonconcecao@yahoo.com.br>
2021-07-30 20:19:27 -0300

    User entity and resource

---
+++ src/main/java/com/adsonweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.adsonweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adsonweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+	User u = new User(1L, "Giselle", "gil@yaho..com", "986543576", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
