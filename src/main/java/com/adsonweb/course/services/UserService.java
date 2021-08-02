184e663e601931a5073d04789c32c50e91fc769b
Adson Santos <adsonconcecao@yahoo.com.br>
2021-08-02 19:26:12 -0300

    Exception handling - findById

--- src/main/java/com/adsonweb/course/services/UserService.java
+++ src/main/java/com/adsonweb/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.adsonweb.course.entities.User;
 import com.adsonweb.course.repositories.UserRepository;
+import com.adsonweb.course.services.exceptions.ResourceNotFountException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFountException(id));
 	}
 	
 	public User insert(User obj) {
